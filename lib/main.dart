dfce0d773cf2707e32635a7c8b79bd15acb4846f
Tanuki <bukubukuu47@gmail.com>
2023-03-03 03:15:19 +0800

    Created first screen, added Android in launch.json

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +10,59 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final ButtonStyle btnStyle = ElevatedButton.styleFrom(
+        textStyle: const TextStyle(
+          fontSize: 20,
+          fontFamily: 'Roboto',
+        ),
+        backgroundColor: const Color.fromRGBO(238, 231, 194, 1),
+        foregroundColor: Colors.black,
+        fixedSize: const Size(300, 67));
+
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor:
-            Color.fromRGBO(152, 161, 127, 1), // set the background color here
+        backgroundColor: const Color.fromRGBO(
+            152, 161, 127, 1), // set the background color here
         body: Center(
-          child: Text('Hello, world!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 96),
+              const Text(
+                'WHO ARE YOU?',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontFamily: 'Roboto',
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 100),
+              ElevatedButton(
+                onPressed: () {},
+                style: btnStyle,
+                child: const Text(
+                  'A Customer',
+                ),
+              ),
+              const SizedBox(height: 60),
+              ElevatedButton(
+                onPressed: () {},
+                style: btnStyle,
+                child: const Text(
+                  'A Technician',
+                ),
+              ),
+              const SizedBox(height: 100),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SvgPicture.asset(
+                  'assets/people_img.svg',
+                  width: 160,
+                  height: 160,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
